fbb05d0604f6f77737e9e9a3b8c5d3a28b0886b6
asabry7 <aaly6995@gmail.com>
2024-12-20 22:24:23 +0200

    Adding Language menu, Test recorder

--- app.py
+++ app.py
@@ -17,23 +17,14 @@ def LoadRecognizer():
     recognizer = read_recognizer(inference_config_or_name=model_name, alt_model_path=model_directory)
     return recognizer
 
-# Title
-st.title("DSP Project")
 
-# Record audio using audio_input
-uploaded_audio = st.audio_input("Record a Reference Utterance")
-# uploaded_audio = st.file_uploader("Record a Reference Utterance", type="wav")
-
-if uploaded_audio is not None:
-    # Load the recognizer
-    recognizer = LoadRecognizer()
-
+def AudioPreprocessor(Utterance):
     # Get the audio data from the BytesIO object
-    audio_data = uploaded_audio.getbuffer()
+    audio_data = Utterance.getbuffer()
 
     # Read the audio data with pydub from the BytesIO object
     audio = AudioSegment.from_wav(io.BytesIO(audio_data))
-    
+
     # Display basic audio info
     st.write(f"Duration: {audio.duration_seconds} seconds")
     st.write(f"Channels: {audio.channels}")
@@ -44,14 +35,44 @@ if uploaded_audio is not None:
         temp_wav_file_path = temp_wav_file.name
         # Export audio data to the temporary .wav file
         audio.export(temp_wav_file, format="wav")
-        
-        # st.write(f"Audio saved temporarily as: {temp_wav_file_path}")
+        return temp_wav_file_path
+    
+# Title
+st.title("DSP Project")
 
-        # Now you can use the temporary file path with the recognizer
+LANGUAGE_ID = "eng"
+language = st.selectbox("Choose Language", ["English", "Egyption Arabic", "Arabic"])
+
+
+# Record audio using audio_input
+ReferenceUtterance = st.audio_input("Record a Reference Utterance")
+# ReferenceUtterance = st.file_uploader("Record a Reference Utterance", type="wav")
+
+if ReferenceUtterance is not None:
+    # Get the audio data from the BytesIO object
+    reference_audio_path = AudioPreprocessor(ReferenceUtterance)
+    TestUtterance = st.audio_input("Record a Test Utterance")
+    if TestUtterance is not None:
+        if language == "Arabic":
+            LANGUAGE_ID = "arb"
+        elif language == "English":
+            LANGUAGE_ID = "eng"
+        elif language == "Egyption Arabic":
+            LANGUAGE_ID = "arz"
+
+        # Load the recognizer
+        recognizer = LoadRecognizer()
+
+
+        test_audio_path = AudioPreprocessor(TestUtterance)
+
         try:
-            result = recognizer.recognize(temp_wav_file_path, lang_id="arb")  # Use "arb" for Arabic
-            st.write("Recognition Result:")
-            st.write(result)
+            ref_result = recognizer.recognize(reference_audio_path, lang_id=LANGUAGE_ID)  # Use "arb" for Arabic
+            test_result = recognizer.recognize(test_audio_path, lang_id=LANGUAGE_ID)  # Use "arb" for Arabic
+            st.write("Reference Utterance Phonemes:")
+            st.write(ref_result)
+            st.write("Test Utterance Phonemes:")
+            st.write(test_result)
         except Exception as e:
             st.error(f"Error during recognition: {e}")
     
